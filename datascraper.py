27bb4f9dd20135ca23021ff42f1577616adbb980
TeteXQ <matteo.roehle@gmail.com>
2023-10-21 13:27:10 +0200

    -m "Second commit"

--- datascraper.py
+++ datascraper.py
@@ -1,14 +1,21 @@
 import requests
-#import pandas as pd
+from bs4 import BeautifulSoup
+import pandas as pd
 
 def getDatafromSite():
     #URL of contest to scrape data from - here Bundesliga 1.
     contestURL = "https://fbref.com/en/comps/20/Bundesliga-Stats"
 
-    #Download site
+    #Find tables
     data = requests.get(contestURL)
-    data.text
-    return data.text
+    soup = BeautifulSoup(data.text, features="html.parser")
+    statsTable = soup.select("table.stats_table")
+    '''links = statsTable.find_all("a")
+    links = [l.get("href") for l in links]'''
+
+    
+    
+    return statsTable
 
 
-print(getDatafromSite())
+
