ff934eac7094278eb52dbbef6421e6d0e8c50db8
Marc <mouket@rhrk.uni-kl.de>
2023-11-27 21:19:12 +0100

    Logik implementiert,fehler mit den Rk als String

--- predictor.py
+++ predictor.py
@@ -19,7 +19,7 @@ class Squad:
             self.xGDp90 = self.__xGDp90()
             self.xG = self.__xG()
             self.xGA = self.__xGA()
-
+            self.mp = self.__mp()
     def __Home(self):
         #Does Squad play at home?
         try:
@@ -82,41 +82,72 @@ class Squad:
         except Exception as e:
             logging.error(f"Couldnt get Rank from {self.name}", e)
             return
-        
+    def __mp(self):
+        #Expected Goals Allowed on Home/Away 
+        try:
+            if self.home:
+                df = leagueTable
+                df = df[(df == self.name).any(axis=1)]
+                return df["MP"].iloc[0]
+            else:
+                df = leagueTable
+                df = df[(df == self.name).any(axis=1)]
+                return df["MP"].iloc[0]
+        except Exception as e:
+            logging.error(f"Couldnt get Rank from {self.name}", e)
+            return
 
-def TestCalculation():
+
+def calculation_v1():
     matches = LeagueData.nextMatches()
 
     homeList = []
-    homePredList = []
-    awayPredList = []
+    homePredGoalList = []
+    awayPredGoalList = []
     awayList = []
 
     for index,row in matches.iterrows():
-        #print(row["Home"])
         homeTeam = Squad(row["Home"])
         awayTeam = Squad(row["Away"])
-        homePred = float(homeTeam.Rk)*((homeTeam.xG)/(awayTeam.xGA))
-        awayPred = float(awayTeam.Rk)*((awayTeam.xG)/(homeTeam.xGA))
+
+        if homeTeam.Rk == '1' or '2' or '3' or '4':
+            homeTeamRkHilfe = 2
+        elif homeTeam.Rk == '5' or '6' or '7' or '8':
+            homeTeamRkHilfe = 1,5
+        elif homeTeam.Rk == '9' or '10' or '11' or '12' or '13':
+            homeTeamRkHilfe = 1,25
+        else: homeTeamRkHilfe = 1
+        
+        if awayTeam.Rk == '1' or '2' or '3' or '4':
+            awayTeamRkHilfe = 2
+        elif awayTeam.Rk == '5' or '6' or '7' or '8':
+            awayTeamRkHilfe = 1,5
+        elif awayTeam.Rk == '9' or '10' or '11' or '12' or '13':
+            awayTeamRkHilfe = 1,25
+        else: awayTeamRkHilfe = 1
+        
+        homeTeam = Squad(row["Home"])
+        awayTeam = Squad(row["Away"])
+        homePred = round(float(homeTeamRkHilfe)*((homeTeam.xG)/(homeTeam.mp)))
+        awayPred = round(float(awayTeamRkHilfe)*((awayTeam.xG)/(awayTeam.mp)))
         
         homeList.append(f"{homeTeam.name}")
-        homePredList.append(homePred)
-        awayPredList.append(awayPred)
+        homePredGoalList.append(homePred)
+        awayPredGoalList.append(awayPred)
         awayList.append(f"{awayTeam.name}")
-        print (homeTeam.name)
-        print (homeTeam.xGA)
-        print (awayTeam.name)
-        print (awayTeam.xG)
 
-    list = [homeList,homePredList,awayPredList,awayList]
+        print(homeTeam.name, homeTeam.Rk, homeTeamRkHilfe)
+        print('')
+        print('VS')
+        print('')
+        print(awayTeam.name, awayTeam.Rk, awayTeamRkHilfe)
+
+    list = [homeList,homePredGoalList,awayPredGoalList,awayList]
 
     df = pd.DataFrame(list,index=["Home","Homescore","Awayscore","Away"]).T
     return(df)
 
 
 
+print(calculation_v1())
 
-print(matches)
-print(TestCalculation())
-glad = Squad("M'Gladbach")
-print(glad.name)
